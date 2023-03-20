0eb76f8e18e450d005e7c140a404b178fd8722d0
viktor0502 <vik.more0502@gmail.com>
2023-03-20 18:05:32 +0200

    1

---
+++ src/main.js
@@ -0,0 +1,42 @@
+var $ = require("jquery");
+require("jquery.marquee");
+
+$('.marquee').marquee({
+	duration: 10000,
+	gap: 50,
+	delayBeforeStart: 0,
+	direction: 'left',
+	duplicated: true
+});
+
+function onEntry(entry) {
+    entry.forEach((change) => {
+      if (change.isIntersecting) {
+        change.target.classList.add("element-show");
+      }
+    });
+  }
+  let options = { threshold: [0.1] };
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll(".element-animation");
+
+  for (let elm of elements) {
+    observer.observe(elm);
+  }
+
+  function smoothScrolling(linkSelector) {
+    const anchors = document.querySelectorAll("." + linkSelector);
+    for (let anchor of anchors) {
+      anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        const blockID = anchor.getAttribute("href").substr(1);
+
+        document.getElementById(blockID).scrollIntoView({
+          behavior: "smooth",
+          block: "start",
+        });
+      });
+    }
+  }
+  smoothScrolling('scroll')
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  watch: true
+};
