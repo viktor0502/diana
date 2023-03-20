059c9e4c9e64fb95f8d7289fa664c88a4730efa0
viktor0502 <vik.more0502@gmail.com>
2023-03-20 19:48:21 +0200

    1

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: './src/main.js',
-  mode: 'development',
+  mode: 'production',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
